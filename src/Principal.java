a7923cf39b9ed5ed59d1325d1f1f773eba807560
Marcelo Otsuki <marceloaires7@gmail.com>
2025-01-01 21:58:29 -0300

    Class Filme e Principal

--- src/Principal.java
+++ src/Principal.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import br.com.alura.screenmatch.modelos.Filme;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+        meuFilme.setIncluidoNoPlano(true);
+//----------------------------------------------------------------------------------------------------------------------
+        meuFilme.exibeFichaTecnica();
+//----------------------------------------------------------------------------------------------------------------------
+        meuFilme.avalia(9);
+        meuFilme.avalia(8);
+        meuFilme.avalia(9);
+//----------------------------------------------------------------------------------------------------------------------
+        System.out.printf("Média de Avaliações: %.2f\n", meuFilme.pegaMedia());
+//----------------------------------------------------------------------------------------------------------------------
+        System.out.println("Total de Avaliações: " + meuFilme.getTotalDeAvaliacoes());
+//----------------------------------------------------------------------------------------------------------------------
     }
 }
---
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,63 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Filme {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+// METHODS -------------------------------------------------------------------------------------------------------------
+    public void exibeFichaTecnica() {
+        System.out.printf("Nome do filme: %s\n", nome);
+        System.out.printf("Ano de lançamento: %s\n", anoDeLancamento);
+    }
+
+    public void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double pegaMedia() {
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+// GETTERS and SETTERS -------------------------------------------------------------------------------------------------
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+}
+
